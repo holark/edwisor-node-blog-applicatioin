af5042c6f7394410a44ae7bf37a28f949bd16eec
holark <rajivnayan99@gmail.com>
2020-01-12 14:20:46 +0530

    routing

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
+// this is needed for importing expressjs into our application
 const express = require('express');
+const fs = require('fs')
 const appConfig = require('./config/appConfig')
+
+// declaring an instance or creating an application instance
 const app = express()
 
-let helloWorldFunction = (req, res) => res.send('Hello World!');
-
-app.get('/', helloWorldFunction)
+// Bootstrap route
+let routesPath = './routes'
+fs.readdirSync(routesPath).forEach(function (file) {
+    if (~file.indexOf('.js')) {
+        console.log("including the following file")
+        console.log(routesPath + '/' + file)
+        let route = require(routesPath + '/' + file);
+        route.setRouter(app);
+    }
+})
 
 app.listen(appConfig.port, () => console.log('Example app listening on port 3000'))
